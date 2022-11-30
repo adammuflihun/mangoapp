716b4cecfc1e2473f2238153d7c358136ad862fb
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2022-11-30 16:34:04 +0700

    s

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import Swiper from 'swiper/bundle';
-import 'swiper/css/bundle';
-
 import { swiperHome } from '$utils/swiperHome';
 
 window.Webflow ||= [];
--- src/utils/swiperHome.ts
+++ src/utils/swiperHome.ts
@@ -1,4 +1,4 @@
-import Swiper from 'swiper/bundle';
+import Swiper from 'swiper';
 import 'swiper/css/bundle';
 
 export const swiperHome = () => {
@@ -16,4 +16,6 @@ export const swiperHome = () => {
     },
   });
   //installation swiper
+
+  console.log(swiper);
 };
