da521fb4472efb82c2dcd2e5cb982f4ee2f87797
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2022-11-30 16:24:46 +0700

    s

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
 import { swiperHome } from '$utils/swiperHome';
 
 window.Webflow ||= [];
--- src/utils/swiperHome.ts
+++ src/utils/swiperHome.ts
@@ -3,26 +3,14 @@ import 'swiper/css/bundle';
 
 export const swiperHome = () => {
   console.log(`Hello ancol!`);
-
   const swiper = new Swiper('.swiper', {
-    // Optional parameters
-
     loop: true,
-
-    // If we need pagination
-    // pagination: {
-    //   el: '.swiper-pagination',
-    // },
-
-    // Navigation arrows
-    // navigation: {
-    //   nextEl: '.swiper-button-next',
-    //   prevEl: '.swiper-button-prev',
-    // },
-
-    // And if we need scrollbar
-    // scrollbar: {
-    //   el: '.swiper-scrollbar',
-    // },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
   });
 };
