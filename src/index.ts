a0168584615b751e06fa9cd4074489fb2db0cd29
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2022-12-04 10:59:35 +0700

    s

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-import { swiperHome } from '$utils/swiperHome';
+import '@splidejs/splide/css';
+import '@splidejs/splide/css/core';
 
-// window.Webflow ||= [];
-// window.Webflow.push(() => {});
-swiperHome();
+import { splideSlide } from '$utils/swiperHome';
+
+window.Webflow ||= [];
+window.Webflow.push(() => {
+  splideSlide();
+});
--- src/utils/swiperHome.ts
+++ src/utils/swiperHome.ts
@@ -1,67 +1,12 @@
-// import Swiper from 'swiper/bundle';
-// import 'swiper/css/bundle';
+// Default theme
+import Splide from '@splidejs/splide';
+import '@splidejs/splide/css';
+import '@splidejs/splide/css/core';
 
-export const swiperHome = () => {
-  //installation swiper
-  const swiper = new Swiper('.swiper', {
-    loop: true,
-    slidesPerView: 2.5,
-    spaceBetween: 0,
-    initialSlide: 3,
-    watchOverflow: true,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2000,
-    },
-
-    navigation: {
-      prevEl: '.swiper-button-prev.prev-swipe',
-      nextEl: '.swiper-button-next.next-swipe',
-    },
-
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    breakpoints: {
-      // when window width is >= 320px
-      320: {
-        slidesPerView: 1.2,
-        spaceBetween: 0,
-
-        initialSlide: 3,
-        watchOverflow: true,
-        centeredSlides: true,
-      },
-      // when window width is >= 480px
-      480: {
-        slidesPerView: 1.2,
-        spaceBetween: 0,
-
-        initialSlide: 3,
-        watchOverflow: true,
-        centeredSlides: true,
-      },
-      // when window width is >= 640px
-      640: {
-        slidesPerView: 1.2,
-        spaceBetween: 0,
-
-        initialSlide: 3,
-        watchOverflow: true,
-        centeredSlides: true,
-      },
-      980: {
-        slidesPerView: 2.2,
-        spaceBetween: 0,
-
-        initialSlide: 3,
-        watchOverflow: true,
-        centeredSlides: true,
-      },
-    },
+export const splideSlide = () => {
+  //installation Splide
+  new Splide('.splide', {
+    type: 'loop',
+    perPage: 3,
   });
-  //installation swiper
-
-  console.log(swiper);
 };
