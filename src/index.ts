c5111f8a15804fa705877195fce0de72b05f760d
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2022-12-04 13:19:01 +0700

    s

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,9 @@ import { swiperslide } from '$utils/swiperHome';
 window.Webflow ||= [];
 window.Webflow.push(() => {
   swiperslide();
+  $('body').append(
+    '<link rel="stylesheet" type="text/css" href="https://mangoapp.vercel.app/dist/index.css">'
+  );
 });
-
-$('body').append(
-  '<link rel="stylesheet" type="text/css" href="https://mangoapp.vercel.app/dist/index.css">'
-);
 
 // $('body').append('<link rel="stylesheet" type="text/css" href="http://localhost:3000/index.css">');
