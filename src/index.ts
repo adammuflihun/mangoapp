be57eba01c1973ea298bf8640c6a5b9f9deda9ea
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2022-12-04 11:00:05 +0700

    s

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import '@splidejs/splide/css';
 import '@splidejs/splide/css/core';
 
-import { splideSlide } from '$utils/swiperHome';
+import { splideSlide } from '$utils/splideSlide';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
